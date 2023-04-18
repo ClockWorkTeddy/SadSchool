bcc1e0113ca14ecb4a5d3093afc8bdab4aa66c66
ivan zhukov <vankaent@live.com>
2023-04-18 18:54:52 +0300

    Add project files.

---
+++ SadSchool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SadSchool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
